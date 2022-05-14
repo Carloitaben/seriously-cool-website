f329c45570841dfd54a5fb020a4ad3894aeda191
Carlo <hola.carlodominguez@gmail.com>
2022-05-14 15:19:13 +0200

    chore(cms): moves schemas to src directory

---
+++ cms/src/desk-structure/index.ts
@@ -0,0 +1,14 @@
+import S from "@sanity/desk-tool/structure-builder"
+
+export default () =>
+  S.list()
+    .title("Content")
+    .items([
+      S.listItem()
+        .title("Settings")
+        .child(S.document().schemaType("settings").documentId("settings")),
+      S.divider(),
+      ...S.documentTypeListItems().filter(
+        (listItem) => !["settings"].includes(listItem.getId())
+      ),
+    ])
