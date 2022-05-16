1bf4f2ed459db7d40b7d10a07c56670775bb9ba5
Carlo <hola.carlodominguez@gmail.com>
2022-05-16 19:51:49 +0200

    feat(web): adds LateralTitle and Svg. Basic home layout

--- web/src/components/Home/Home.tsx
+++ web/src/components/Home/Home.tsx
@@ -1,7 +1,26 @@
 import type { FC } from "react"
 
+import LateralTitle from "~/components/LateralTitle"
+import { emojiLogo } from "~/components/Svg"
+
 const Home: FC = () => {
-  return <div className="bg-red-300 h-full pt-16">Home</div>
+  return (
+    <div className="bg-red-300 h-full pt-16 flex  pb-12">
+      <LateralTitle href="/about">
+        About myself
+        <br />
+        over here
+      </LateralTitle>
+      <div className="flex-1 flex items-center justify-center px-12">
+        {emojiLogo}
+      </div>
+      <LateralTitle href="/projects" flip>
+        Projects are
+        <br />
+        this way
+      </LateralTitle>
+    </div>
+  )
 }
 
 export default Home
---
+++ web/src/components/LateralTitle/LateralTitle.tsx
@@ -0,0 +1,26 @@
+import { Link } from "@remix-run/react"
+import type { FC } from "react"
+
+type Props = {
+  children: string | string[]
+  href: `/${string}`
+  flip?: boolean
+}
+
+const LateralTitle: FC<Props> = ({ children, flip = false, href }) => {
+  return (
+    <div className="h-full text-5xl text-center uppercase whitespace-nowrap px-container">
+      <div className="flex items-center justify-center w-24 underline h-full">
+        <Link
+          prefetch="intent"
+          to={href}
+          className={flip ? "rotate-90" : "-rotate-90"}
+        >
+          {children}
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default LateralTitle
