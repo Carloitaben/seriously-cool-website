7b5a825a0c8f5fb6e4503555c3d6a424f8e19344
Carlo <hola.carlodominguez@gmail.com>
2022-05-23 19:14:18 +0200

    feat(web): adds useIntersectionObserver hook

---
+++ web/src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,38 @@
+import type { RefObject } from "react"
+import { useEffect, useRef, useState } from "react"
+
+type IntersectionObserverHookConfig = IntersectionObserverInit & {
+  callback?: (entry: IntersectionObserverEntry) => void
+  disconnect?: boolean
+}
+
+/**
+ * Creates an `IntersectionObserver` that will observe the provided reference.
+ */
+export default function useIntersectionObserver<T extends RefObject<any>>(
+  reference: T,
+  config: IntersectionObserverHookConfig = {}
+) {
+  const { disconnect = false, callback, ...intersectionObserverInit } = config
+
+  const [intersecting, setIntersecting] = useState(false)
+  const observer = useRef<IntersectionObserver>()
+
+  useEffect(() => {
+    if (reference.current && !disconnect) {
+      observer.current = new IntersectionObserver(([entry]) => {
+        setIntersecting(entry.isIntersecting)
+        if (callback) callback(entry)
+      }, intersectionObserverInit)
+
+      observer.current.observe(reference.current)
+
+      return () => {
+        if (observer.current) observer.current.disconnect()
+        observer.current = undefined
+      }
+    }
+  }, [callback, disconnect, intersectionObserverInit, reference])
+
+  return intersecting
+}
