65de1a48bcba7a5b987a0aba7844cbd0abf674a7
Carlo <hola.carlodominguez@gmail.com>
2022-05-27 00:09:27 +0200

    feat(web): adds Appear to ProjectDetailBlockText

--- web/src/components/ProjectDetailBlockText/ProjectDetailBlockText.tsx
+++ web/src/components/ProjectDetailBlockText/ProjectDetailBlockText.tsx
@@ -2,15 +2,18 @@ import type { FC } from "react"
 
 import type { ProjectBlockRichText } from "~/types"
 
+import Appear from "../Appear"
 import TextBlock from "../TextBlock"
 
 type Props = ProjectBlockRichText
 
 const ProjectDetailBlockText: FC<Props> = ({ textRaw }) => {
   return (
-    <div className="tablet:text-2xl desktop:py-20 desktop:text-2xl desktop:col-span-3 desktop:col-end-7 col-span-4 -my-2 py-12 text-lg">
-      <TextBlock>{textRaw}</TextBlock>
-    </div>
+    <Appear>
+      <div className="tablet:text-2xl desktop:py-20 desktop:text-2xl desktop:col-span-3 desktop:col-end-7 col-span-4 -my-2 py-12 text-lg">
+        <TextBlock>{textRaw}</TextBlock>
+      </div>
+    </Appear>
   )
 }
 
