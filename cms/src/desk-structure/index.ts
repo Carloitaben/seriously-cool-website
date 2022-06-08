492f93a70adf5387dd816d0a183a55a41b93a831
Carlo <hola.carlodominguez@gmail.com>
2022-06-08 15:22:58 +0200

    chore(cms): removes duplicated desk structure

--- cms/src/desk-structure/index.ts
+++ cms/src/desk-structure/index.ts
@@ -1,4 +1,22 @@
 import S from "@sanity/desk-tool/structure-builder"
+import Iframe from "sanity-plugin-iframe-pane"
+
+import resolveProductionUrl from "./resolveProductionUrl"
+
+export const getDefaultDocumentNode = ({ schemaType }) => {
+  if (schemaType === "project") {
+    return S.document().views([
+      S.view.form(),
+      // Including the iframe pane, with a function to create the url
+      S.view
+        .component(Iframe)
+        .options({ url: (doc) => resolveProductionUrl(doc, "/projects/") })
+        .title("Preview"),
+    ])
+  }
+
+  return S.document()
+}
 
 export default () =>
   S.list()
