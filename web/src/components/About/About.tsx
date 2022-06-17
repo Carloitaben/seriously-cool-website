f52d11cdb4a848081fe50f1b2fbd605ff1676c5f
Carlo <hola.carlodominguez@gmail.com>
2022-06-17 20:47:54 +0200

    feat(web): adds Appear to About

--- web/src/components/About/About.tsx
+++ web/src/components/About/About.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react"
 import useRootData from "~/hooks/useRootData"
+import Appear from "../Appear"
 
 import AboutFirstParagraph from "./AboutFirstParagraph"
 import AboutLastParagraph from "./AboutLastParagraph"
@@ -20,15 +21,17 @@ const About: FC<Props> = () => {
 
   return (
     <div className="desktop:fixed desktop:top-0 desktop:left-0 desktop:h-screen desktop:w-1/2 desktop:overflow-y-auto desktop:overscroll-contain desktop:pr-1 desktop:pl-2 desktop:pb-24 desktop:pt-2">
-      <section
-        className="rounded-4xl desktop:px-16 desktop:pb-16 desktop:text-5xl flex-1 px-10 pb-10"
-        style={style}
-      >
-        <AboutNavbar />
-        <AboutFirstParagraph />
-        <AboutParagraphs />
-        <AboutLastParagraph />
-      </section>
+      <Appear>
+        <section
+          className="rounded-4xl desktop:px-16 desktop:pb-16 desktop:text-5xl flex-1 px-10 pb-10"
+          style={style}
+        >
+          <AboutNavbar />
+          <AboutFirstParagraph />
+          <AboutParagraphs />
+          <AboutLastParagraph />
+        </section>
+      </Appear>
     </div>
   )
 }
