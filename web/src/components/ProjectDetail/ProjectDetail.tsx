88e2d5c8000156c96997bbaa81abab392edf424b
Carlo <hola.carlodominguez@gmail.com>
2022-06-17 19:22:30 +0200

    chore(web): moves project detail JSX to custom component

---
+++ web/src/components/ProjectDetail/ProjectDetail.tsx
@@ -0,0 +1,50 @@
+import type { FC } from "react"
+import { useState } from "react"
+
+import type { ProjectDetailLoaderData } from "~/routes/projects/$slug"
+
+import Appear from "~/components/Appear"
+import AppearText from "~/components/AppearText"
+import TextBlock from "~/components/TextBlock"
+import ProjectDetailClientLocation from "~/components/ProjectDetailClientLocation"
+import ProjectDetailBlocks from "~/components/ProjectDetailBlocks"
+
+type Props = {
+  project: ProjectDetailLoaderData["project"]
+}
+
+const ProjectDetail: FC<Props> = ({ project }) => {
+  const [finishedTitleAnimation, setFinishedTitleAnimation] = useState(false)
+
+  return (
+    <div className="h-full overflow-y-auto pb-20">
+      <div className="px-18 my-24 grid grid-cols-8 gap-x-8 text-2xl">
+        <div className="col-span-3 max-w-xl text-5xl leading-tight">
+          <h1>
+            <AppearText
+              onAnimationStart={() => setFinishedTitleAnimation(true)}
+            >
+              {project.title}
+            </AppearText>
+          </h1>
+        </div>
+        <Appear
+          animate={finishedTitleAnimation}
+          className="col-span-4 col-end-9 max-w-2xl"
+        >
+          <TextBlock>{project.descriptionRaw}</TextBlock>
+        </Appear>
+      </div>
+      <ProjectDetailBlocks>{project.blocks}</ProjectDetailBlocks>
+      <div className="px-container grid grid-cols-6 gap-x-2 text-2xl">
+        <ProjectDetailClientLocation
+          client={project.clientRaw}
+          location={project.location}
+          year={project.year}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ProjectDetail
--- web/src/routes/projects/$slug.tsx
+++ web/src/routes/projects/$slug.tsx
@@ -1,24 +1,17 @@
-import { useState } from "react"
-import { getFileAsset } from "@sanity/asset-utils"
 import type { LoaderFunction } from "@remix-run/node"
 import { useLoaderData } from "@remix-run/react"
+import { getFileAsset } from "@sanity/asset-utils"
 
 import type { GetProjectQuery, GetProjectQueryVariables } from "~/types"
 import { filterSanityDocumentDrafts, isSanityPreview } from "~/utils"
 import { client, dataset, projectId, GET_PROJECT } from "~/graphql"
-
-import Appear from "~/components/Appear"
-import AppearText from "~/components/AppearText"
-import TextBlock from "~/components/TextBlock"
-import ProjectDetailClientLocation from "~/components/ProjectDetailClientLocation"
-import ProjectDetailBlocks from "~/components/ProjectDetailBlocks"
+import ProjectDetail from "~/components/ProjectDetail"
 
 export type ProjectDetailLoaderData = {
   project: GetProjectQuery["allProject"][number]
 }
 
 export const loader: LoaderFunction = async ({
-  context,
   params,
   request,
 }): Promise<ProjectDetailLoaderData> => {
@@ -67,35 +60,5 @@ export const loader: LoaderFunction = async ({
 export default function Route() {
   const { project } = useLoaderData<ProjectDetailLoaderData>()
 
-  const [finishedTitleAnimation, setFinishedTitleAnimation] = useState(false)
-
-  return (
-    <div className="h-full overflow-y-auto pb-20">
-      <div className="px-18 my-24 grid grid-cols-8 gap-x-8 text-2xl">
-        <div className="col-span-3 max-w-xl text-5xl leading-tight">
-          <h1>
-            <AppearText
-              onAnimationStart={() => setFinishedTitleAnimation(true)}
-            >
-              {project.title}
-            </AppearText>
-          </h1>
-        </div>
-        <Appear
-          animate={finishedTitleAnimation}
-          className="col-span-4 col-end-9 max-w-2xl"
-        >
-          <TextBlock>{project.descriptionRaw}</TextBlock>
-        </Appear>
-      </div>
-      <ProjectDetailBlocks>{project.blocks}</ProjectDetailBlocks>
-      <div className="px-container grid grid-cols-6 gap-x-2 text-2xl">
-        <ProjectDetailClientLocation
-          client={project.clientRaw}
-          location={project.location}
-          year={project.year}
-        />
-      </div>
-    </div>
-  )
+  return <ProjectDetail project={project} />
 }
