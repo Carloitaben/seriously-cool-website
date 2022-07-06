ce49b5426323360a5c969a6539792b4388f9d4c4
Carlo <hola.carlodominguez@gmail.com>
2022-07-06 16:05:01 +0200

    feat(web): adds back button animation
    Closes #58

--- web/src/components/SlidingText/BackButton.tsx
+++ web/src/components/SlidingText/BackButton.tsx
@@ -1,8 +1,22 @@
 import type { FC } from "react"
 import { Link, useLocation } from "@remix-run/react"
+import type { Variants } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
 
 import useRootData from "~/hooks/useRootData"
 import { close } from "~/components/Svg"
+import { lightboxTransition } from "~/utils"
+
+const variants: Variants = {
+  hide: {
+    width: "0%",
+    height: "0%",
+  },
+  show: {
+    width: "auto",
+    height: "auto",
+  },
+}
 
 const BackButton: FC = () => {
   const location = useLocation()
@@ -11,18 +25,27 @@ const BackButton: FC = () => {
     theme: { colors },
   } = useRootData()
 
-  if (location.pathname === "/") return null
-
   return (
-    <div className="flex-none">
-      <Link
-        to="/"
-        className="ml-4 flex h-16 w-16 items-center justify-center rounded-full border-2 border-current"
-        style={{ background: colors.background }}
-      >
-        {close}
-      </Link>
-    </div>
+    <AnimatePresence initial={false}>
+      {location.pathname === "/" ? null : (
+        <motion.div
+          className="flex-none"
+          initial="hide"
+          animate="show"
+          exit="hide"
+          variants={variants}
+          transition={lightboxTransition}
+        >
+          <Link
+            to="/"
+            className="ml-4 flex h-16 w-16 items-center justify-center rounded-full border-2 border-current"
+            style={{ background: colors.background }}
+          >
+            {close}
+          </Link>
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
 
