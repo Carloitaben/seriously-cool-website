9fcd86ee5e3a3bd0ad9425fc07725a10ecad8e7e
Carlo <hola.carlodominguez@gmail.com>
2022-07-20 19:17:03 +0200

    feat(web): modifies native cursor visibility only when custom cursor is loaded

--- web/src/components/Cursor/Cursor.tsx
+++ web/src/components/Cursor/Cursor.tsx
@@ -1,14 +1,28 @@
 import type { FC } from "react"
-import { useState } from "react"
+import { useRef, useState, useEffect } from "react"
 
 import useOnMatchMedia from "~/hooks/useOnMatchMedia"
 
 import CursorComponent from "./CursorComponent"
 
+const style = "* { cursor: none !important; }"
+
 const Cursor: FC = () => {
   const [enableCursor, setEnableCursor] = useState(false)
+  const sheetRuleRef = useRef<number>()
 
   useOnMatchMedia("(pointer: fine)", setEnableCursor)
+
+  useEffect(() => {
+    const sheet = document.styleSheets[0]
+
+    if (enableCursor) {
+      sheetRuleRef.current = sheet.insertRule(style, sheet.cssRules.length)
+    } else if (sheetRuleRef.current) {
+      sheet.deleteRule(sheetRuleRef.current)
+      sheetRuleRef.current = undefined
+    }
+  }, [enableCursor])
 
   if (!enableCursor) return null
 
